753a2196f7c3ec0eb81dafd48361d26e12c1e90f
Matthias Gerstner <Matthias.Gerstner@nefkom.net>
2023-11-16 20:52:56 +0100

    scrollback buffer: support scrolling using the mouse wheel
    On the main screen use the mouse wheel (BUTTON4/BUTTON5) for scrolling
    up and down the history buffer by fixed number of lines (slow
    scrolling). Pressing SHIFT additionally will scroll by half a screen
    (fast scrolling).
    
    On the alt screen continue injecting the special escape sequences that
    allow scrolling in less, vi etc.
    
    Drop the special escape sequence when pressing SHIFT though ("\033[5;2~"
    and "\033[6;2~"). These synthesize SHFIT + PAGEUP/PAGEDOWN which doesn't
    help us anymore.

--- src/nst_config.cxx
+++ src/nst_config.cxx
@@ -24,6 +24,7 @@ std::vector<MouseShortcut> get_mouse_shortcuts(Nst &nst) {
 
 	auto &tty = nst.tty();
 	auto &wsys = nst.wsys();
+	auto &term = nst.term();
 
 	auto ttysend = [&](const std::string_view s) {
 		tty.write(s, TTY::MayEcho{true});
@@ -31,13 +32,40 @@ std::vector<MouseShortcut> get_mouse_shortcuts(Nst &nst) {
 
 	using xpp::Button;
 
+	auto scrollHistoryUp = [&term, ttysend](bool shift) {
+		// on the alt screen we inject special escape sequences that
+		// are used by programs like less and vim for scrolling.
+		if (term.onAltScreen()) {
+			ttysend("\031");
+		} else {
+			if (shift)
+				term.scrollHistoryUpByPage(0.5);
+			else
+				term.scrollHistoryUpByLines(+5);
+		}
+	};
+
+	auto scrollHistoryDown = [&term, ttysend](bool shift) {
+		// see above
+		if (term.onAltScreen()) {
+			ttysend("\005");
+		} else {
+			if (shift)
+				term.scrollHistoryDownByPage(0.5);
+			else
+				term.scrollHistoryDownByLines(+5);
+		}
+	};
+
 	return {
-		//             mask              button           function                              release
+		//             mask              button           function                                         release
 		MouseShortcut{ Mask{Mod::ANY},   Button::BUTTON2, std::bind(&WindowSystem::pasteSelection, &wsys), true },
-		MouseShortcut{ Mask{Mod::SHIFT}, Button::BUTTON4, std::bind(ttysend, "\033[5;2~"),      false },
-		MouseShortcut{ Mask{Mod::ANY},   Button::BUTTON4, std::bind(ttysend, "\031"),           false },
-		MouseShortcut{ Mask{Mod::SHIFT}, Button::BUTTON5, std::bind(ttysend, "\033[6;2~"),      false },
-		MouseShortcut{ Mask{Mod::ANY},   Button::BUTTON5, std::bind(ttysend, "\005"),           false },
+		// these generate sequences used by less/vi for scrolling
+		MouseShortcut{ Mask{Mod::SHIFT}, Button::BUTTON4, std::bind(scrollHistoryUp, true),                false },
+		MouseShortcut{ Mask{Mod::SHIFT}, Button::BUTTON5, std::bind(scrollHistoryDown, true),              false },
+		// regular scrolling via mouse wheel
+		MouseShortcut{ Mask{Mod::ANY},   Button::BUTTON4, std::bind(scrollHistoryUp, false),               false },
+		MouseShortcut{ Mask{Mod::ANY},   Button::BUTTON5, std::bind(scrollHistoryDown, false),             false },
 	};
 }
 

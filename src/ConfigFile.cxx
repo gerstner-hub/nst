bd7c8c3d8e5a220316a542a861ef976dae4a8bdd
Matthias Gerstner <Matthias.Gerstner@nefkom.net>
2024-02-28 19:04:01 +0100

    ConfigFile: support unicode strings and proper string quoting style

--- src/ConfigFile.cxx
+++ src/ConfigFile.cxx
@@ -1,5 +1,7 @@
 // C++
+#include <codecvt>
 #include <fstream>
+#include <locale>
 
 // cosmos
 #include "cosmos/string.hxx"
@@ -13,44 +15,113 @@ ConfigFile::ConfigFile(cosmos::ILogger &logger) :
 		m_logger{logger} {
 }
 
+std::optional<std::string> ConfigFile::asString(const std::string key) const {
+	auto it = m_items.find(key);
+	if (it == m_items.end())
+		return std::nullopt;
+
+	auto value = it->second;
+
+	if (!unquoteStringValue(value))
+		return std::nullopt;
+
+	return toNarrowString(value);
+}
+
 void ConfigFile::parse(const std::string_view path) {
-	std::ifstream fs{path.data()};
+	std::wifstream fs{path.data()};
+	fs.imbue(std::locale("en_US.utf8"));
 
 	if (!fs) {
 		return;
 	}
 
 	size_t linenr = 1;
-	std::string line;
+	std::wstring line;
 	while (std::getline(fs, line).good()) {
 		parseLine(path, linenr, line);
 		linenr++;
 	}
 }
 
-void ConfigFile::parseLine(const std::string_view file, const size_t linenr, std::string &line) {
+void ConfigFile::parseLine(const std::string_view file, const size_t linenr, std::wstring &line) {
+	auto parseError = [=](const std::string_view error) {
+		m_logger.error() << "ConfigFile parse error in " << file << ":" << linenr << ": " << error << "\n";
+	};
+
 	cosmos::strip(line);
 
 	if (line.empty())
 		return;
 	// comment
-	else if (line[0] == '#')
+	else if (line[0] == L'#')
 		return;
 
-	auto sep = line.find('=');
+	auto sep = line.find(L'=');
 
 	if (sep == line.npos) {
-		m_logger.error() << "ConfigFile parse error in " << file << ":" << linenr << ": missing '=' separator\n";
+		parseError("missing '=' seperator");
 		return;
 	}
 
 	auto key = line.substr(0, sep);
 	auto value = line.substr(sep+1);
 
-	cosmos::strip(key);
 	cosmos::strip(value);
 
-	m_items[key] = value;
+	if (auto narrow_key = toNarrowString(cosmos::stripped(key)); narrow_key != std::nullopt) {
+		m_items[*narrow_key] = value;
+	} else {
+		parseError("key contains non-ascii characters");
+		return;
+	}
+}
+
+std::optional<std::string> ConfigFile::toNarrowString(const std::wstring &s) const {
+	std::string ret;
+
+	for (const auto ch: s) {
+		if (ch >= 128) {
+			m_logger.error() << "ConfigFile parse error, non-ascii characters found in configuration value \""
+				<< toUTF8(s) << "\"\n";
+			return std::nullopt;
+		}
+
+		ret.push_back(static_cast<char>(ch));
+	}
+
+	return ret;
+}
+
+bool ConfigFile::unquoteStringValue(std::wstring &s) const {
+	if (s.size() < 2 || s[0] != L'"' || s.back() != L'"') {
+		m_logger.error() << "ConfigFile parse error, badly quoted string value encountered in \"" << toUTF8(s) << "\"\n";
+		return false;
+	}
+
+	s = s.substr(1, s.size() - 2);
+
+	for (size_t idx = 0; idx < s.size(); idx++) {
+		if (const auto ch = s[idx]; ch == L'\\') {
+			s.erase(idx, 1);
+			if (idx >= s.size()) {
+				m_logger.error() << "ConfigFile parse error, stray \\ in \"" << toUTF8(s) << "\"\n";
+				return false;
+			}
+
+			if (const auto esc = s[idx]; esc != L'"' && esc != '\\') {
+				m_logger.error() << "ConfigFile parse error, unsupported backslash escape in \"" << toUTF8(s) << "\"\n";
+				return false;
+			}
+		}
+	}
+
+	return true;
+}
+
+std::string ConfigFile::toUTF8(const std::wstring &s) const {
+    std::wstring_convert<std::codecvt_utf8<wchar_t>> myconv;
+    return myconv.to_bytes(s);
 }
 
 } // end ns
--- src/ConfigFile.hxx
+++ src/ConfigFile.hxx
@@ -23,6 +23,14 @@ namespace nst {
  * The class's API offers access to each individual configuration key and also
  * converting the values into typical types used in NST like strings, integers
  * and colors.
+ *
+ * Basic unicode support is necessary for configuration string values e.g. for
+ * the word seperator characters. Therefore strings configuration values have
+ * to be quoted like "this". Minimal backslash escapes are supported for
+ * escaping double quotes '\"' and the backslash character itself '\\'.
+ *
+ * For configuration keys only ASCII characters are allowed. No multiline
+ * continuation is supported.
  **/
 class ConfigFile {
 public: // functions
@@ -38,22 +46,46 @@ public: // functions
 	 **/
 	void parse(const std::string_view path);
 
-	std::optional<std::string> asString(const std::string key) const {
-		auto it = m_items.find(key);
-		if (it == m_items.end())
-			return std::nullopt;
-
-		return it->second;
-	}
+	/// Attempt to access the given configuration key and returns its ASCII string value.
+	/**
+	 * If the key does not exist or has parsing / type error then
+	 * std::nullopt is returned. On parsing / type errors the error will
+	 * be logged. Error conditions include:
+	 *
+	 * - badly quoted string.
+	 * - bad backslash escapes in string.
+	 * - string contains non-ASCII characters.
+	 **/
+	std::optional<std::string> asString(const std::string key) const;
 
 protected: // functions
 
-	void parseLine(const std::string_view file, const size_t linenr, std::string &line);
+	void parseLine(const std::string_view file, const size_t linenr, std::wstring &line);
+
+	/// Parse a string value from the given string in-place.
+	/**
+	 * String values have to be quoted and may contain escape sequences.
+	 * This function parses and removes these elements. On any parsing
+	 * errors diagnostics will be output via logging and \c false is
+	 * returned and the contents of \c s are undefined. Otherwise \c true
+	 * is returned and \c s contains the properly unquoted string value.
+	 **/
+	bool unquoteStringValue(std::wstring &s) const;
+
+	/// Turns a wide string into a plain ASCII string.
+	/**
+	 * If any non-ASCII characters are contained then std::nullopt is
+	 * returned and an error is logged.
+	 **/
+	std::optional<std::string> toNarrowString(const std::wstring &s) const;
+
+	/// Encode a wide string as UTF8 and return the result as a std::string.
+	std::string toUTF8(const std::wstring &s) const;
 
 protected: // data
 
 	cosmos::ILogger &m_logger;
-	std::map<std::string, std::string> m_items;
+	std::map<std::string, std::wstring> m_items;
 };
 
 } // end ns
